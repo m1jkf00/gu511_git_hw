547523d7f37664c93230dacb0561a6f76b8e06af
jkf44 <jkf44@georgetown.edu>
2020-11-11 10:04:57 -0800

    dspipeline: adding usage instructions

--- dspipeline.py
+++ dspipeline.py
@@ -8,7 +8,7 @@ Created: 2018-10-19
 
 Description:
     simple data science pipeline functions and a demo which uses these tools to
-    model a freely available datasetd
+    model a freely available dataset
 
 Usage:
     import dspipeline
